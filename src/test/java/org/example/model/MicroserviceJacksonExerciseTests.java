da19cf7e6cd4c8536a024179a1359aaf1a37d737
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-11-11 16:29:12 +0100

    bugs fixed and serialization/deserialization tests passed

---
+++ src/test/java/org/example/model/MicroserviceJacksonExerciseTests.java
@@ -0,0 +1,14 @@
+package org.example.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MicroserviceJacksonExerciseTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
